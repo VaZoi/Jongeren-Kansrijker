57d443ddeddac95fca68d2364a3a16e74f9006ce
VZ <131747053+VaZoi@users.noreply.github.com>
2024-02-10 03:01:19 +0100

    Update db.php

--- Project/db.php
+++ Project/db.php
@@ -13,7 +13,7 @@ class database {
         $this->username = 'root';
         $this->password = '';
         $this->database = 'school';
-        $this->port = 3307;
+        $this->port = 3306;
     
         $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->database";
         try {
